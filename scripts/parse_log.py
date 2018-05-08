6a5226e2ea89e42c257f3e56a24024c4fbeffbb4
Jianwei Chen <jianweichen@utexas.edu>
2018-05-08 18:10:27 -0500

    add scripts to parse LOG

---
+++ scripts/parse_log.py
@@ -0,0 +1,21 @@
+import subprocess
+import sys
+import csv
+log_file = sys.argv[1]
+result = subprocess.check_output("grep 'Compaction level' {} -A 3".format(log_file), shell=True)
+entries = result.split("--\n")
+
+with open(log_file + "_res.csv", "w") as f:
+    writer = csv.writer(f)
+    writer.writerow(["level", "guards", "file_num", "file_size", "time"])
+    for entry in entries:
+        print(entry)
+        lines = entry.split("\n")
+        level = lines[0].split(' ')[-1]
+        file_size = int(lines[1].split(' ')[5]) + int(lines[2].split(' ')[5])
+        file_num = int(lines[1].split(' ')[3]) + int(lines[2].split(' ')[3])
+        guards = int(lines[1].split(' ')[7]) + int(lines[2].split(' ')[7])
+        guards = int(lines[1].split(' ')[7]) + int(lines[2].split(' ')[7])
+        micros = lines[3].split(' ')[-2]
+        writer.writerow([level, guards, file_num, file_size, micros])
+        print([level, guards, file_num, file_size, micros])
