62a7afd14e50a5f2909b241e159164367ae4283b
Zeyuan Hu <ferrishu3886@gmail.com>
2018-05-08 21:49:18 -0700

    Update rep2.sh

--- scripts/rep2.sh
+++ scripts/rep2.sh
@@ -1,9 +1,9 @@
 #!/bin/sh -vx
 
 # Try to replicate the experiement "Impact of Empty Guards" in PebblesDB paper
-max_iter=20             # num of iterations to run
+max_iter=0              # num of iterations to run
 i=0         
-num=100000000           # num rows to insert
+num=1000000             # num rows to insert
 value_size=512          # row size (byte)
 reads=$(( $num / 2 ))   # num reads
 db_bench=../db_bench    # path to db_bench executable
@@ -11,29 +11,29 @@ db_bench=../db_bench    # path to db_bench executable
 WORKLOADS=${1:-0}       # which workload to run
 case $WORKLOADS in
     0)
-        benchmarks_args=fillseq,readrandom,seekrandom,deleteseq,stats,emptyGuards
+        benchmarks_args=fillseq,readseq,seekrandom,deleteseq,stats,emptyGuards
         ;;
     1)
-        benchmarks_args=fillseq,readreverse,seekrandom,deleteseq,stats,emptyGuards
+        benchmarks_args=fillseq,readrandom,seekrandom,deleteseq,stats,emptyGuards
         ;;
     2)
-        benchmarks_args=fillseq,readmissing,seekrandom,deleteseq,stats,emptyGuards
+        benchmarks_args=fillseq,readreverse,seekrandom,deleteseq,stats,emptyGuards
         ;;
     3)
-        benchmarks_args=fillseq,readhot,seekrandom,deleteseq,stats,emptyGuards
+        benchmarks_args=fillseq,readmissing,seekrandom,deleteseq,stats,emptyGuards
         ;;
     4)
+        benchmarks_args=fillseq,readhot,seekrandom,deleteseq,stats,emptyGuards
+        ;;
+    5)
         benchmarks_args=fillrandom,readseq,readrandom,readreverse,readmissing,readhot,seekrandom,stats,emptyGuards
         ;;
     *)
         ;;
 esac
 
-db_path=/var/local/pebblesdbtest-1000
-if [ ! -d $db_path ]; then
-    echo "db_path not exist"
-    exit
-fi
+db_path=./pebblesdbtest-1000
+mkdir -p $db_path
 
 # We insert 20M key-value pairs (with keys from 0 to 20M, value size: 512B, dataset size: 10 GB), perform 10M read operations on the data, and delete all keys.
 ./$db_bench --benchmarks=$benchmarks_args --num=$num --value_size=$value_size --reads=$reads --base_key=0 --db=$db_path
